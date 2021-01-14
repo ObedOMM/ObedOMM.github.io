1b58fa6f9414d6033ed1574116110213afa4efe1
ObedOMM <53335727+ObedOMM@users.noreply.github.com>
2021-01-14 23:21:40 +0100

    Add files via upload

---
+++ precache-manifest.c05569ecfac9303c948b1ef0dd961931.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a17083f0aa337f5a8f0f2af95f3611",
+    "url": "/index.html"
+  },
+  {
+    "revision": "845b649f8953b098766d",
+    "url": "/static/css/main.d3a56717.chunk.css"
+  },
+  {
+    "revision": "290d5ebf40d54e3354fb",
+    "url": "/static/js/2.9817da54.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.9817da54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845b649f8953b098766d",
+    "url": "/static/js/main.20bf5f16.chunk.js"
+  },
+  {
+    "revision": "dcc534e5adf1fce6f480",
+    "url": "/static/js/runtime-main.5f677427.js"
+  },
+  {
+    "revision": "6f82017092b3f62c335565b373a29482",
+    "url": "/static/media/KronaOne-Regular.6f820170.ttf"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/static/media/Questrial-Regular.37c8a1aa.ttf"
+  }
+]);
---
+++ script.js
@@ -0,0 +1,4 @@
+// form
+(function(){
+  emailjs.init("user_4FE7oV7W5ZH5a1TM5DCdS"); // Obtain your user ID at the dashboard https://dashboard.emailjs.com/integration
+})();
